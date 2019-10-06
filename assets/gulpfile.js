6223ac1d24ab174aa1fa738e95e634eae410c4b6
hltree <kiona.n31@icloud.com>
2019-10-06 14:42:23 +0900

    update: join gulp

---
+++ assets/gulpfile.js
@@ -0,0 +1,11 @@
+const
+    gulp = require('gulp'),
+    notify = require('gulp-notify'),
+    pug = require('gulp-pug'),
+    sass = require('gulp-sass');
+
+gulp.task('pug', () => {
+    return (
+        gulp.src()
+    );
+});
