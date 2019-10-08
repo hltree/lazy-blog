d45dbbdfc19ff18a8c55deb0ea1aef97a4d300a0
hltree <kiona.n31@icloud.com>
2019-10-09 01:56:56 +0900

    update: setupFiles

--- manage.py
+++ manage.py
@@ -1,2 +1,4 @@
-from src import app
-app.run(host='127.0.0.1', port=5000, debug=True)
+from src.app import app
+
+if __name__ == '__main__':
+    app.run()
--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask.ext.sqlAlchemy import SQLAlchemy
-
-app = flask(__name__)
-app.config.from_object('src.config')
-
-db = SQLAlchemy(app)
-
-import src.views
