72c4d069b41f14be52ca9ccaaf2a57e1ef1a5d41
hltree <kiona.n31@icloud.com>
2019-10-13 23:53:12 +0900

    update: initpy

---
+++ fblog/__init__.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATA
+    )
