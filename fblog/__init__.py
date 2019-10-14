c9c284efc4f6d439e96ff7205ef00126b345df50
hltree <kiona.n31@icloud.com>
2019-10-14 23:45:37 +0900

    update: read config

--- fblog/__init__.py
+++ fblog/__init__.py
@@ -5,5 +5,15 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATA
+        DATABASE=os.path.join(app.instance_path, 'fblog.sqlite'),
     )
+
+    # configを読み込む
+    app.config.from_pyfile('config.py', silent=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    
