90f498a4e3b11fdc86440e41bc9e90a666c0696b
hltree <kiona.n31@icloud.com>
2019-10-30 20:02:51 +0900

    update: addIndexPage

--- app/routes.php
+++ app/routes.php
@@ -8,3 +8,10 @@ $customErrorHandler = function (
     $response = $app->getResponseFactory()->createResponse();
     return $this->get('view')->render($response, '404.html');
 };
+
+$errorHandler = $app->addErrorMiddleware(true, true, true);
+$errorHandler->setDefaultErrorHandler($customErrorHandler);
+
+$app->get('/', function($request, $response){
+    return $this->get('view')->render($response, 'index.html');
+})->setName('index');
