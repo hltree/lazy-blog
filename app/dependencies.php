314d1cdaace640ba346d402073170794cc3fab4e
hltree <kiona.n31@icloud.com>
2019-11-09 23:09:05 +0900

    fix: for PDO

--- app/dependencies.php
+++ app/dependencies.php
@@ -3,18 +3,12 @@
 use App\Setting;
 
 $settings = new Setting\Settings();
-$container->set('db', function ($settings) {
-    $db = $settings->db;
-    var_dump($settings);
-    $dsn = "mysql:host={$db['user']}@{$db['host']};port={$db['port']};dbname={$db['dbname']}charset=utf8;";
-    try {
-        $pdo = new PDO($dsn, $db['password']);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    }
-    catch(PDOException $e) {
-        echo 'Connection failed: ' . $e->getMessage();
-    }
-});
-
-$db = $container->get('db');
+$db = $settings->db['db'];
+$dsn = "mysql:host={$db['host']};port={$db['port']};dbname={$db['dbname']};charset=utf8;";
+try {
+    $pdo = new PDO($dsn, $db['user'], $db['password']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
