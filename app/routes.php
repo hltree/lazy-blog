8ad91c4742794c70e5b264890043527389d21d81
hltree <kiona.n31@icloud.com>
2019-11-10 23:51:54 +0900

    update: twig

--- app/routes.php
+++ app/routes.php
@@ -12,6 +12,20 @@ $customErrorHandler = function (
 $errorHandler = $app->addErrorMiddleware(true, true, true);
 $errorHandler->setDefaultErrorHandler($customErrorHandler);
 
-$app->get('/', function($request, $response){
+$app->get('/', function ($request, $response) {
     return $this->get('view')->render($response, 'index.html');
 })->setName('index');
+
+
+$app->get('/login', function ($request, $response, $args) {
+    $session = $_SESSION['login'];
+    $session = $session ? htmlspecialchars($session) : false;
+    if ($session === true) {
+        return $this->get('view')->render($response, 'admin.html');
+    } else {
+        return $this->get('view')->render($response, 'login.twig', [
+            'user' => $args['user'],
+            'pass' => $args['pass']
+        ]);
+    }
+})->setName('login');
