3eff018b5c420defb0acae22d7c0874b91469bb7
hltree <kiona.n31@icloud.com>
2019-11-24 19:06:50 +0900

    update: add Styke

--- app/manager/models.py
+++ app/manager/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=128)
     published = models.DateTimeField()
-    image = models.ImageField(upload_to = 'uploads/')
+    image = models.ImageField(upload_to = 'uploads')
     content = models.TextField()
