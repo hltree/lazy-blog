440299e429a208587576b4e464fc9faa1681755b
hltree <kiona.n31@icloud.com>
2019-11-24 17:56:10 +0900

    update: Add Index

--- app/manager/views.py
+++ app/manager/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView
+from manager.models import *
 
-# Create your views here.
+#get_object_or_404()
+
+class index(TemplateView):
+    template_name = "index.html"
+
+    def get(self, request, *args, **kwargs):
+        context = super(index, self).get_context_data(**kwargs)
+        return render(self.request, self.template_name, context)
