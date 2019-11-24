b200b7f2a5dbf2c9eaf672a02cbfbc2227ea8d95
hltree <kiona.n31@icloud.com>
2019-11-24 16:50:40 +0900

    update: init django

---
+++ app/manager/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    published = models.DateTimeField()
+    image = models.ImageField(upload_to = 'uploads/')
+    content = models.TextField()
