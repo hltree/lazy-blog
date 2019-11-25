01ee62f3d33bcda670abd4afad3c2242a2e4189a
hltree <kiona.n31@icloud.com>
2019-11-26 01:04:15 +0900

    update

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls.static import static
-from django.views.static import serve
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
@@ -24,7 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', manager_view.index.as_view(), name='index'),
     path('post/<post_id>', manager_view.post_detail.as_view(), name='post_detail'),
-] + static(settings.MEDIA_URL, document_root=settings.BASE_DIR)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.BASE_DIR)
 
 if settings.DEBUG:
     import debug_toolbar
