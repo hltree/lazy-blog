8764037bf69d87392b61985727ae2538fcff50ac
hltree <kiona.n31@icloud.com>
2019-11-28 11:20:32 +0900

    fix: adjustment views

--- manager/views.py
+++ manager/views.py
@@ -8,7 +8,7 @@ class index(TemplateView):
     def get(self, request, *args, **kwargs):
         context = super(index, self).get_context_data(**kwargs)
 
-        posts = Post.objects.all()
+        posts = Post.objects.order_by('-published')
         context['posts'] = posts
 
         return render(self.request, self.template_name, context)
