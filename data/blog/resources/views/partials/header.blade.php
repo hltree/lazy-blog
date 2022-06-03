2337efb8c8f75052338ef0af60f7553039a6db37
hltree <35206336+hltree@users.noreply.github.com>
2022-06-03 11:10:11 +0900

    chore: headerにナビを追加

--- data/blog/resources/views/partials/header.blade.php
+++ data/blog/resources/views/partials/header.blade.php
@@ -2,5 +2,43 @@
     <div class="inner">
         <h1 class="title">{{ config('app.name') }}</h1>
         <p>ここには何か追加シマす。。予定。</p>
+
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ms-auto">
+            <!-- Authentication Links -->
+            @guest
+                @if (Route::has('login') && config('app.show_login_form'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login', ['key' => config('app.login_key')]) }}">{{ __('Login') }}</a>
+                    </li>
+                @endif
+
+                @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+
+                        <a class="dropdown-item" href="{{ route('password.confirm', ['key' => config('app.login_key')]) }}">{{ __('Confirm') }}</a>
+                    </div>
+                </li>
+            @endguest
+        </ul>
     </div>
 </header>
