d35fda91e921078e961f7f07ae1a11c02e5506d9
hltree <35206336+hltree@users.noreply.github.com>
2022-06-03 09:45:06 +0900

    chore: シーダーにユーザー追加の処理を定義

--- data/blog/database/seeders/DatabaseSeeder.php
+++ data/blog/database/seeders/DatabaseSeeder.php
@@ -14,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        /**
+         * Create Admin
+         */
+         \App\Models\User::factory()->create([
+             'name' => 'Admin',
+             'email' => env('APP_EMAIL'),
+         ]);
     }
 }
