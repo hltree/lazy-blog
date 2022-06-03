21c7fc930bb56d475bc8057301220ac8b05e401a
hltree <35206336+hltree@users.noreply.github.com>
2022-06-03 15:40:22 +0900

    chore: サイドバーにダッシュボードへのリンクを追加。投稿一覧に何も表示することのないときの処理を作成

--- data/blog/resources/views/post/list.blade.php
+++ data/blog/resources/views/post/list.blade.php
@@ -2,21 +2,26 @@
 @section('content')
     <div class="posts">
         <div class="card">
-{{--            タイトル必要ならコメントアウト解除--}}
-{{--            <div class="page-title card-body text-center">--}}
-{{--                <h4 class="card-title m-b-0">Posts</h4>--}}
-{{--            </div>--}}
+            {{--            タイトル必要ならコメントアウト解除--}}
+            {{--            <div class="page-title card-body text-center">--}}
+            {{--                <h4 class="card-title m-b-0">Posts</h4>--}}
+            {{--            </div>--}}
             <ul class="list-style-none">
+                @if (0 === count($posts))
+                    <li class="d-flex">{{ __('コンテンツがありません') }}</li>
+                @endif
                 @foreach($posts as $post)
                     <li class="d-flex no-block card-body @auth enable-edit-button @endauth">
                         <a href="{{ route('post.show', ['id' => $post->id]) }}">
                             <div>
-                                <div class="time">{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $post->created_at)->format('Y.m.d') }}</div>
+                                <div
+                                    class="time">{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $post->created_at)->format('Y.m.d') }}</div>
                                 <div class="m-b-0 font-medium p-0">{{ $post->title }}</div>
                             </div>
                         </a>
 
-                        <a class="btn btn-primary" href="{{ route('post.edit', ['id' => $post->id]) }}">{{ __('Edit') }}</a>
+                        <a class="btn btn-primary"
+                           href="{{ route('post.edit', ['id' => $post->id]) }}">{{ __('Edit') }}</a>
                     </li>
                 @endforeach
             </ul>
