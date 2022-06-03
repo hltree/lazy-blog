c95c83329469bdb3d878077f3ec3908215d8f8c7
hltree <35206336+hltree@users.noreply.github.com>
2022-06-03 11:26:27 +0900

    chore: ダッシュボードのコンテンツを修正

--- data/blog/resources/views/home.blade.php
+++ data/blog/resources/views/home.blade.php
@@ -8,13 +8,10 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <a href="{{ route('post.newPost') }}">{{ __('新規投稿を作成する') }}</a>
+                </div>
+                <div class="card-body">
+                    <a href="{{ route('post.list') }}">{{ __('投稿一覧') }}</a>
                 </div>
             </div>
         </div>
